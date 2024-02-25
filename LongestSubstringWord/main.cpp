408344eeabe41e54d4f0d6f8e465683cc882f73d
Jaime Cuartas <jaime.cuartas1228@gmail.com>
2024-02-25 17:47:12 +1100

    Given a string s, find the length of the longest substring  without repeating characters.

--- LongestSubstringWord/main.cpp
+++ LongestSubstringWord/main.cpp
@@ -6,9 +6,29 @@
 //
 
 #include <iostream>
+#include <map>
+#include <string>
+
+int lengthOfLongestSubstring(std::string s) {
+    std::map<char, int> map;
+    int maxRepeats = 0;
+    int repeats = 0;
+    int initIndex = 0;
+    for(int i=0; i<s.size(); i++){
+        char c = s[i];
+        std::map<char, int>::iterator indexFound = map.find(c);
+        if(indexFound!=map.end() && initIndex<=(indexFound->second+1)){
+            initIndex = indexFound->second+1;
+            repeats = i - initIndex;
+        }
+        map.insert_or_assign(c, i);
+        repeats++;
+        if(repeats>maxRepeats) maxRepeats = repeats;
+    }
+    return maxRepeats;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << lengthOfLongestSubstring("pwwkew") << "\n";
     return 0;
 }
--- PracticeCodeProblems/longestSubstringWord.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  longestSubstringWord.cpp
-//  PracticeCodeProblems
-//
-//  Created by Jaime Cuartas Granada on 25/2/2024.
-//
-
-#include "longestSubstringWord.hpp"
