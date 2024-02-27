3857bcfca588ec3640310d26dde5c85487559b05
Jaime Cuartas <jaime.cuartas1228@gmail.com>
2024-02-28 00:18:12 +1100

    Longest Palindromic Substring completed

--- LongestPalindromicSubstring/main.cpp
+++ LongestPalindromicSubstring/main.cpp
@@ -8,22 +8,55 @@
 #include <iostream>
 #include <string>
 
+void printMatrix (bool a[][11]){
+    for(int i=0; i<11; i++){
+        for(int j=0; j<11; j++){
+            std::cout << a[i][j] << ",";
+        }
+        std::cout << "\n";
+    }
+}
+
 std::string longestPalindrome(std::string s) {
     int n = static_cast<int>(s.size());
-    int sTable [n][n];
     if (n==0) return 0;
-    for(int i=0; i<n; i++){
-        sTable[0][i] = (s[0] == s[i])? 1 : 0;
-    }
-    for(int i=1; i<n; i++){
-        for(int j=0; j<n; j++){
-            sTable[i][j] = sTable[i-1][j] + ((s[i] == s[n-i-1])? 1 : 0);
+    if (n==1) return s;
+    //the substring from 0 to 0 , 1 to 1, n to n is alway palindrome, because it is one letter
+    bool isPalindromeTable [n][n];
+    for(int i=0; i<11; i++){
+        for(int j=0; j<11; j++){
+            isPalindromeTable[i][j] = false;
         }
     }
+    for(int i=0; i<n; i++){
+        isPalindromeTable[i][i] = true;
+    }
+    //start with palindrome of size 1
+    int maxPalindrome = 1;
+    int startMaxPalindrome = 0;
+    for(int i=0; i<(n-1); i++){
+        isPalindromeTable[i][i+1] = s[i] == s[i+1];
+        if(isPalindromeTable[i][i+1]){
+            maxPalindrome = 2;
+            startMaxPalindrome = i;
+        }
+    }
+    for (int i=2; i<n; i++) {
+        for(int j=0; j<(n-i); j++){
+            if(isPalindromeTable[j+1][j+i-1]){
+                isPalindromeTable[j][j+i] = (s[j] == s[j+i]);
+                if(isPalindromeTable[j][j+i] && (i+1)>maxPalindrome){
+                    maxPalindrome = i+1;
+                    startMaxPalindrome = j;
+                }
+            }
+        }
+    }
+    return s.substr(startMaxPalindrome, maxPalindrome);
 }
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << longestPalindrome("aacabdkacaa") << "\n";
     return 0;
 }
