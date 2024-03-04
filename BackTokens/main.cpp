67c1d003d3df76d9557efdf17b3ea197a1ff4ddc
Jaime Cuartas <jaime.cuartas1228@gmail.com>
2024-03-04 19:12:13 +1100

    bagOfTokensScore finished

---
+++ BackTokens/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BackTokens
+//
+//  Created by Jaime Cuartas Granada on 4/3/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
