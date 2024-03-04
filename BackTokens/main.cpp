fdbd79a45374192b402d8e334b40b046a751adf7
Jaime Cuartas <jaime.cuartas1228@gmail.com>
2024-03-04 19:12:41 +1100

    back of tokens and its readme done

--- BackTokens/main.cpp
+++ BackTokens/main.cpp
@@ -6,6 +6,29 @@
 //
 
 #include <iostream>
+#include <vector>
+
+int bagOfTokensScore(std::vector<int>& tokens, int power) {
+    int n = static_cast<int>(tokens.size());
+    int score = 0;
+    int maxScore = 0;
+    int start = 0;
+    int end = n-1;
+    std::sort (tokens.begin(), tokens.end());
+    while (start<=end) {
+        if(power >= tokens[start]){
+            power -= tokens[start];
+            score++;
+            start++;
+            if(score > maxScore) maxScore = score;
+        } else if (score>0) {
+            power += tokens[end];
+            score--;
+            end--;
+        } else break;
+    }
+    return maxScore;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
