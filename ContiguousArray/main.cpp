81010e49cd6104ec9cdbf90f272101f230ab138e
Jaime Cuartas <jaime.cuartas1228@gmail.com>
2024-03-17 12:11:11 +1100

    findMaxLength improve reading

--- ContiguousArray/main.cpp
+++ ContiguousArray/main.cpp
@@ -8,32 +8,27 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <map>
 
 int findMaxLength(std::vector<int>& nums) {
     size_t n = nums.size();
     if(n==0) return 0;
-    std::unordered_map<int, int> lastSimilar;
+    std::map<int, int> lastSimilar;
     lastSimilar[0] = -1;
-    std::unordered_map<int, int>::iterator lastIndex;
+    std::map<int, int>::iterator lastIndex;
     int maxLenght = 0;
     int currentLenght = 0;
     nums[0] = nums[0]==1? 1: -1;
     lastSimilar[nums[0]] = 0;
     for(int i=1; i<n; i++){
-        if(nums[i]){
-            nums[i] = nums[i-1] + 1;
-        }else{
-            nums[i] = nums[i-1] - 1;
-        }
+        nums[i] = nums[i-1] + ((nums[i])? 1: -1);
         lastIndex = lastSimilar.find(nums[i]);
-        if(lastIndex == lastSimilar.end()){
-            lastSimilar[nums[i]] = i;
-        }else{
+        if(lastIndex != lastSimilar.end()){
             currentLenght = i - lastIndex->second;
-            if(currentLenght>maxLenght){
-                maxLenght = currentLenght;
-            }
+            if(currentLenght>maxLenght) maxLenght = currentLenght;
+            continue;
         }
+        lastSimilar[nums[i]] = i;
     }
     return maxLenght;
 }
